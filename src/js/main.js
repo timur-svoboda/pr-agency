82cf0985953f2ed7d6c2c2eb10fd2c27b1f11eae
Timur Svoboda <svobodatimur@gmail.com>
2022-02-05 14:12:16 +0200

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Burger
+  const burger = document.querySelector(".burger-menu");
   
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("burger-menu_cross");
+
+    const lines = Array.from(burger.querySelectorAll(".burger-menu__line"));
+    lines.forEach(line => line.classList.toggle("burger-menu__line_cross"))
+
+    document.querySelector(".header__nav").classList.toggle("header__nav_open");
+  });
 });
